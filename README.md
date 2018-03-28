ecec6e10a480e6e7d10a25a61bb603734db2599c
Nick <nickjljames@gmail.com>
2018-03-27 23:38:15 -0700

    updates to work for debian

--- README.md
+++ README.md
@@ -1,9 +1,13 @@
-sets a fresh mac up with a bunch of that sweet sweet automation.
+sets a fresh mac or debian box up with a bunch of that sweet sweet automation.
+
+You get:
+- python setup with pyenv
+- neovim with a shit load of dope confiuration
+- node installed with nvm
+- a bunch of environment installs with brew/apt to make dev easier
 
 ### Get set up:
-1. `git clone git@github.com:steph-query/nicksmac.git`
+1. `git clone git@github.com:steph-query/nick-dev.git`
 2. `cd nicksmac`
 3. `./setup.sh`
-4. `./cobalt_theme.sh`
-5. `ansible-playbook nicksmac.yml`
 
--- setup.sh
+++ setup.sh
@@ -5,16 +5,16 @@ if ! [ -e ~/.ssh ] ; then
 	mkdir ~/.ssh
 fi
 
-# make a github key
+## make a github key
 if ! [ -e ~/.ssh/github ] ; then
-    ssh-keygen -t rsa -N "" -f ~/.ssh/github
+    ssh-keygen -t rsa -N "" -f ~/.ssh/github_key
 	# add the key to ssh
 	ssh-add ~/.ssh/github
 	# make ssh config file
 	touch ~/.ssh/config
 fi
 
-# put git key in ssh config
+## put git key in ssh config
 if [  -z "$(cat ~/.ssh/config | grep github)" ] ; then
     echo "Host github.com
 	HostName github.com
@@ -23,19 +23,30 @@ if [  -z "$(cat ~/.ssh/config | grep github)" ] ; then
     >> ~/.ssh/config
 fi
 
-# install homebrew
-if [ -z "$(which brew)" ] ; then
-	/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)" \
-	&& brew update
-fi
-
-# install python + ansible
-brew install python
-pip install ansible
-
 # install ohmyzsh
-if ! [ echo "$ENV" -eq "/bin/bash" ] ; then
+if ! [ echo "$SHELL" -eq "/bin/zsh" ] ; then
 	sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 fi
 
+
+if [[ "$OSTYPE" == "darwin"* ]]; then
+  # install homebrew
+  if [ -z "$(which brew)" ] ; then
+    /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)" \
+    && brew update
+  fi
+  brew install python
+elif [[ "$OSTYPE" == "debian"* ]]; then
+  sudo apt-get update -yqq
+  sudo apt-get install python-pip
+fi
+
+pip install -U pip
+pip install ansible --user
+
+ansible-playbook playbooks/main.yml
+
+./cobalt_theme.sh
+
 exit 0
+
